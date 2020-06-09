4c34b91d99f86b2318b236817161fd975c6a7728
Robert Gadon <rgadon107@gmail.com>
2020-06-09 14:03:06 -0500

    Filter 'genesis_initial_layouts' hook.
    - Return the layout configuration to 'full-width-content' only.

---
+++ lib/functions/layout.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  Filter the Layout configuration in the child theme.
+ *
+ * @package    modifiedGenesisSample\Functions
+ * @since      1.0.0
+ * @author     Robert A. Gadon
+ * @link       http://spiralwebdb.com
+ * @license    GNU General Public License 2.0+
+ */
+
+namespace modifiedGenesisSample\Functions;
+
+add_filter( 'genesis_initial_layouts', __NAMESPACE__ . '\filter_layout_config' );
+/**
+ * Filter the layout configuration
+ *
+ * @param string $config The config file to look for (not including .php file extension).
+ *
+ * @return array The config data.
+ * @since 1.0.0
+ *
+ */
+function filter_layout_config() {
+	return genesis_get_config( 'layouts' );
+}
+ 
